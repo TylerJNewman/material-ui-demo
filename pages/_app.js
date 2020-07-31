848400f7748d3f5ef4f092d2bc36fa6e4d4b12ca
Tyler Newman <tylerjnewman18@gmail.com>
2020-07-31 13:17:00 -0700

    Move layout to top level component to allow navigation

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from '../src/theme'
+import { Layout } from '../src/layout'
 
 export default function MyApp (props) {
   const { Component, pageProps } = props
@@ -25,7 +26,9 @@ export default function MyApp (props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </>
   )
